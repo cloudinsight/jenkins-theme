063b60f1b4195bec77e424d77ee49170e3392c1b
龙天 <wyvernnot@gmail.com>
2016-08-19 11:21:48 +0800

    Update jenkins-ga.js

--- jenkins-ga.js
+++ jenkins-ga.js
@@ -10,5 +10,5 @@
   m.parentNode.insertBefore(a, m)
 })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-ga('create', 'UA-79575996-2', 'auto');
+ga('create', 'UA-79575996-1', 'auto');
 ga('send', 'pageview');
